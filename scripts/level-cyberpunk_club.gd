90eaea3f08c458b351a8917f8451a364a927536f
Chase Little <chaselittle@Chases-MacBook-Pro-2.local>
2024-03-21 17:03:07 -0700

    dialogue test in scene

--- scripts/level-cyberpunk_club.gd
+++ scripts/level-cyberpunk_club.gd
@@ -5,19 +5,3 @@ var d_resource = preload("res://assets/dialogue/level-1.dialogue")
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	DialogueManager.show_example_dialogue_balloon(d_resource)
-	
-# Callback function to handle dialogue events
-func on_dialogue_event(event):
-	match event.name:
-		"line_displayed":
-			# Trigger animations or other actions based on which line of dialogue is displayed
-			var current_line = event.data["line"]
-			match current_line:
-				"Ceia: Great! So this lesson will be of great help":
-					# Trigger animation for this line of dialogue
-					# Example: $AnimationPlayer.play("ceia_animation")
-					_animation_player.play("component_wizard-hand-animation.tscn")
-				"Ciea: Now, we have to use our new variables to make a program.":
-					# Trigger animation for this line of dialogue
-					# Example: $AnimationPlayer.play("ciea_animation")
-					_animation_player.play("component_wizard-hand-animation.tscn")
