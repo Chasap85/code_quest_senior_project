b9ac53fc827f3aa87d0b2f6eb19268f7556e0499
Chase Little <chaselittle@Chases-MacBook-Pro-2.local>
2024-03-24 22:31:08 -0700

    added new assets and styled dialogue box

--- scripts/level-cyberpunk_club.gd
+++ scripts/level-cyberpunk_club.gd
@@ -4,4 +4,5 @@ var d_resource = preload("res://assets/dialogue/level-1.dialogue")
 @onready var _animation_player = $AnimationPlayer
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	DialogueManager.show_example_dialogue_balloon(d_resource)
+	print("Play scene")
+	#DialogueManager.show_example_dialogue_balloon(d_resource)
