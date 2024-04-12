2557736ff2e4614349681e116b3533c791ca7f91
Cash <74753263+Cassius-CS@users.noreply.github.com>
2024-04-11 19:27:25 -0600

    Load in prompts and starter code from JSON

--- scripts/PopupHandler.gd
+++ scripts/PopupHandler.gd
@@ -1,11 +1,14 @@
 extends Node
 
+@onready var code_edit = $"../../.."
+
 func _ready():
 	pass
 
 func run_popup(status_id: int):
 	match status_id:
 		3:
+			code_edit._emit_accepted_status()
 			print("Accepted") # Debug Statement
 			# TODO: Make Accepted GUI - Proceed Level
 		4:
