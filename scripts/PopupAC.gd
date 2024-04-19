e2b717c5fa60b8858c104ab511fd48144891246a
Cassius-CS <cashs0426@gmail.com>
2024-04-18 21:36:54 -0600

    Implement AnimationHandler, example use in level1 script

--- scripts/PopupAC.gd
+++ scripts/PopupAC.gd
@@ -1,6 +1,7 @@
 extends Node2D
 
 @onready var data_handler = $"../DataHandler"
+@onready var animation_handler = $"../AnimationHandler"
 
 func _ready():
 	pass
@@ -10,4 +11,6 @@ func answer_accepted():
 
 func _on_continue_pressed():
 	hide()
+	animation_handler._destroy_active_animation()
+	animation_handler._play_current_animation()
 	data_handler.load_next_section()
