3e5b1f9f9dac5faf5c4fc5cf5ee6dd085206d404
Cash <74753263+Cassius-CS@users.noreply.github.com>
2024-05-09 09:48:20 -0600

    Make const to reduce volume intensity

--- scripts/menus/options.gd
+++ scripts/menus/options.gd
@@ -1,6 +1,7 @@
 extends Control
 const MAIN_MENU_SCENE = "res://level-scenes/main_menu.tscn"
 const MASTER_VOL_IDX = 0
+const VOLUME_REDUCE_FACTOR = 10
 
 @onready var volume_slider = $MarginContainer/VBoxContainer/HBoxContainer/VolumeSlider
 
@@ -15,11 +16,11 @@ func _on_back_button_pressed():
 	get_tree().change_scene_to_file(MAIN_MENU_SCENE)
 
 func _set_volume_slider(value: float):
-	volume_slider.value = value * 10
+	volume_slider.value = value * VOLUME_REDUCE_FACTOR
 	pass
 
 func _on_volume_slider_changed(new_volume: float):
 	AudioServer.set_bus_volume_db(
 		MASTER_VOL_IDX,
-		linear_to_db(new_volume / 10)
+		linear_to_db(new_volume / VOLUME_REDUCE_FACTOR)
 	)
