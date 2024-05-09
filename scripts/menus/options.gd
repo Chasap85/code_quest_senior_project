abde4449d214336440c8bf9f56d3f79a963a5664
Cash <74753263+Cassius-CS@users.noreply.github.com>
2024-05-08 18:59:42 -0600

    Add volume slider to options

--- scripts/menus/options.gd
+++ scripts/menus/options.gd
@@ -1,9 +1,25 @@
 extends Control
-
 const MAIN_MENU_SCENE = "res://level-scenes/main_menu.tscn"
+const MASTER_VOL_IDX = 0
+
+@onready var volume_slider = $MarginContainer/VBoxContainer/HBoxContainer/VolumeSlider
 
 func _ready():
+	var initial_volume: float = db_to_linear(
+		AudioServer.get_bus_volume_db(MASTER_VOL_IDX)
+	)
+	_set_volume_slider(initial_volume)
 	pass
 
 func _on_back_button_pressed():
 	get_tree().change_scene_to_file(MAIN_MENU_SCENE)
+
+func _set_volume_slider(value: float):
+	volume_slider.value = value * 10
+	pass
+
+func _on_volume_slider_changed(new_volume: float):
+	AudioServer.set_bus_volume_db(
+		MASTER_VOL_IDX,
+		linear_to_db(new_volume / 10)
+	)
